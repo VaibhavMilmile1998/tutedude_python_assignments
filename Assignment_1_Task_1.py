dd9b6f7e30302e4ce1d068031e38cbfdfb71ad6d
Vaibhav Milmile <165415873+VaibhavMilmile1998@users.noreply.github.com>
2025-04-27 22:14:25 +0530

    Add files via upload

---
+++ Assignment_1_Task_1.py
@@ -0,0 +1,13 @@
+#Taking input for 1st number
+a=int(input("Enter the first number: "))
+#Taking input for 2nd  number
+b=int(input("Enter the second number: "))
+
+#Addition
+print("Addition: ",a+b)
+#Subtraction
+print("Subtraction: ",a-b)
+#Multiplication
+print("Multiplication: ",a*b)
+#Division
+print("Division: ",a/b)
---
+++ Assignment_1_Task_2.py
@@ -0,0 +1,7 @@
+#Taking input for First Name
+a=input("Enter the first name: ")
+#Taking input for Last Name
+b=input("Enter the last name: ")
+
+# Prints a personalized greeting message using the full name.
+print("Hello,"+ ' ' + a+ ' '+ b+'!' + " Welcome "+ "to "+"the "+ "python "+"program")
